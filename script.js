7f8ac2dcc9804c6938fa9eb0e5789997cce30159
MariaDawes <maria.c.dawes@gmail.com>
2019-12-19 13:27:34 -0600

    Day of the week and data added using moment.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
-console.log(moment().format('dddd')); 
-console.log(moment().format("MMM Do YY"));
+// console.log(moment().format('dddd')); 
+// console.log(moment().format("MMM Do YY"));
 
-moment().format('dddd'); 
-moment().format("MMM Do YY");
+// moment().format('dddd'); 
+// moment().format("MMM Do YY");
+
+var weekDay = (moment().format('dddd'));
+document.getElementById("currentDay").innerHTML = weekDay
+
+var weekDate = (moment().format("MMM Do YYYY"));
+document.getElementById("currentDate").innerHTML = weekDate
