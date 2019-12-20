e521161da30cbea98a5c22c01f2de9a6d53046c7
MariaDawes <maria.c.dawes@gmail.com>
2019-12-19 23:21:39 -0600

    format correct, it accepts written task input and saves. When I add colors interfears with the task inout

--- script.js
+++ script.js
@@ -1,12 +1,130 @@
+    var planner = [{
+        time: "9",
+        message: "",
+        save: false,
+    },
+    {
+        time: "10",
+        message: "",
+        save: false,
+    },
+    {
+        time: "11",
+        message: "",
+        save: false,
+    },
+    {
+        time: "12",
+        message: "",
+        save: false,
+    },
+    {
+        time: "13",
+        message: "",
+        save: false,
+    },
+    {
+        time: "14",
+        message: "",
+        save: false,
+    },
+    {
+        time: "15",
+        message: "",
+        save: false,
+    },
+    {
+        time: "16",
+        message: "",
+        save: false,
+    },
+    {
+        time: "17",
+        message: "",
+        save: false,
+    }
 
-// console.log(moment().format('dddd')); 
-// console.log(moment().format("MMM Do YY"));
+    ]
 
-// moment().format('dddd'); 
-// moment().format("MMM Do YY");
+    function addTask(){
 
-var weekDay = (moment().format('dddd'));
-document.getElementById("currentDay").innerHTML = weekDay
+        $(document).ready(function () {
+        
+            $(".saveBtn").on("click", function () {
+        
+                var task = $(this).siblings(".description").val()
 
-var weekDate = (moment().format("MMM Do YYYY"));
-document.getElementById("currentDate").innerHTML = weekDate
+                console.log("task", task);
+        
+                //get the value the user types and store it in the task    
+            })
+        })
+
+
+    }
+
+
+
+    function addDate() {
+
+        // This function adds day of the week and date to the jumbotron. 
+
+        var weekDay = (moment().format('dddd, MMM Do YYYY'));
+        document.getElementById("currentDay").innerHTML = weekDay
+    }
+
+
+    function applyColor() {
+
+        // Gets the current time and compares to the each of the times
+        // in the planner to add the correct past, present and future colors.
+
+        var timeofDay = (moment().format('h'));
+        console.log("time of day:", timeofDay);
+
+        if (timeofDay == 1) {
+            (timeofDay = 13);
+        };
+        if (timeofDay == 2) { 
+            (timeofDay = 14);
+        };
+        if (timeofDay == 3) {
+            timeofDay = 15;
+        };
+        if (timeofDay == 4) {
+            timeofDay = 16;
+        };
+        if (timeofDay == 5) {
+            timeofDay = 17;
+        };
+
+        for (var i = 0; i < planner.length; i++) {
+
+            var plannerRepo = planner[i];
+            console.log("plannerRepo.time", plannerRepo.time);
+            
+            if (timeofDay == plannerRepo.time) {
+                    $(".description");
+                    $(".description").addClass("present");
+                    alert("present");
+            }
+                    else if (timeofDay < plannerRepo.time) {
+                        $(".description");
+                        $(".description").addClass("future");
+                        alert("future");
+                    }
+                        else if (timeofDay > plannerRepo.time) {
+                            //add gray to the box;
+                            $(".description");
+                            $(".description").addClass("past");
+                            alert("past");              
+                        }
+
+        }
+
+    }
+
+
+    addDate();
+    applyColor();
+    addTask();
